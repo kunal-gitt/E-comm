a1f9bffabd8ba49ac8b11bb51e63b59080c79ec3
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-25 17:02:14 +0530

    added dummy data using map

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,61 @@
 import React from "react";
 import "./Home.css";
 
+const DUMMY_List = [
+  {
+    id: 1,
+    date: "july 16",
+    country: "DETROIT,MI",
+    location: "DTE ENERGY MUSIC",
+  },
+  {
+    id: 2,
+    date: "july 19",
+    country: "TORONTO,ON",
+    location: "BUDWEISER STAGE",
+  },
+  {
+    id: 3,
+    date: "july 22",
+    country: "BRISTOW,VA",
+    location: "JUGGY KUBE LIVE",
+  },
+  {
+    id: 4,
+    date: "JULY 29",
+    country: "PHOENIX,AZ",
+    location: "AK-CHIN PAVILION",
+  },
+  {
+    id: 5,
+    date: "AUG 2",
+    country: "LAS VEGAS,NV",
+    location: "T-MOBILE ARENA",
+  },
+
+  {
+    id: 6,
+    date: "AUG 7",
+    country: "CONCORD,CA",
+    location: "CONCORD PAVILION",
+  },
+];
+
 const Home = () => {
+  const Locationlist = DUMMY_List.map((location) => {
+    return (
+      <div>
+        <li className="li">
+          <span className="show">{location.date}</span>{" "}
+          <span>{location.country}</span>
+          <span>{location.location}</span>
+          <button className="btn-1">Buy Tickets</button>
+        </li>
+        <hr className="hr" />
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="div">
@@ -12,44 +66,7 @@ const Home = () => {
         <h1 className="tours">Tours</h1>
       </div>
       <div>
-        <ul>
-          <li className="li">
-            <span className="show">July 16</span> <span>DETROIT,MI</span>
-            <span>DTE ENERGY MUSIC</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-          <li className="li">
-            <span className="show">July 19</span> <span>TORONTO,ON</span>
-            <span>BUDWEISER STAGE</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-          <li className="li">
-            <span className="show">JULY 22</span> <span>BRISTOW,VA</span>
-            <span>JUGGY KUBE LIVE</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-          <li className="li">
-            <span className="show">JULY 29</span> <span>PHOENIX,AZ</span>
-            <span>AK-CHIN PAVILION</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-          <li className="li">
-            <span className="show">AUG 2</span> <span>LAS VEGAS,NV</span>
-            <span>T-MOBILE ARENA</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-          <li className="li">
-            <span className="show">AUG 7</span> <span>CONCORD,CA</span>
-            <span>CONCORD PAVILION</span>
-            <button className="btn-1">Buy Tickets</button>
-          </li>
-          <hr className="hr" />
-        </ul>
+        <ul>{Locationlist}</ul>
       </div>
 
       <h2 className="h2">THE GENERICS</h2>
