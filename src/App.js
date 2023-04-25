209471e2e4b6d8abe2bfa0b601e3ed76c319f6b6
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-25 17:03:28 +0530

    Deliverables Added Contact US Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./Components/Layout/Header";
 
 import "./App.css";
-import Meals from "./Components/Products/Products";
+
 import Cart from "./Components/Cart/Cart";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./About";
+
 import Products from "./Components/Products/Products";
 import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./About";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -20,15 +22,33 @@ function App() {
     setCartIsShown(false);
   };
 
+  const addDetailsHandler = async (details) => {
+    const response = await fetch(
+      "https://react-http-bbcb8-default-rtdb.firebaseio.com/details.json",
+      {
+        method: "POST",
+        body: JSON.stringify(details),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+  };
+
   return (
     <div>
       <BrowserRouter>
         {cartIsShown && <Cart onClose={hideCartHandler} />}
         <Header onShowCart={showCartHandler} />
+
         <Routes>
           <Route path="/" element={<Products />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/About" element={<About />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/contact"
+            element={<Contact onAddDetails={addDetailsHandler} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Contact.js
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import "./Contact.css";
+
+const Contact = (props) => {
+  const NameRef = useRef("");
+  const EmailRef = useRef("");
+  const PhoneRef = useRef("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const details = {
+      name: NameRef.current.value,
+      email: EmailRef.current.value,
+      phone: PhoneRef.current.value,
+    };
+
+    props.onAddDetails(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" ref={NameRef} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" ref={EmailRef}></input>
+      </div>
+      <div>
+        <label htmlFor="phoneno">Phone no. </label>
+        <input type="number" id="phoneno" ref={PhoneRef} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Contact;
